29c4863be5dc8857dff213f2d77c4c1c89e2639a
Melvin Chew <melvincv15@gmail.com>
2022-02-04 16:41:10 -0700

    Created User and AccountService classes

---
+++ src/main/java/ca/sait/lab4/services/AccountService.java
@@ -0,0 +1,26 @@
+package ca.sait.lab4.services;
+
+import ca.sait.lab4.models.User;
+
+/**
+ *
+ * @author Melvin Chew
+ */
+public class AccountService {
+    public User login(String username, String password) {
+
+        if (username.equals("abe") && password.equals("password")) {
+        
+            return new User(username, null);
+
+        } else if (username.equals("barb") && password.equals("password")) {
+
+            return new User(username, null);
+
+        } else {
+            
+            return null;
+
+        }
+    }
+}
