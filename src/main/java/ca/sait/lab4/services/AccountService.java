0299fad6d9479ec02df42101462404d07f94111b
Melvin Chew <melvincv15@gmail.com>
2022-02-04 17:44:45 -0700

    Removed Lines

--- src/main/java/ca/sait/lab4/services/AccountService.java
+++ src/main/java/ca/sait/lab4/services/AccountService.java
@@ -8,7 +8,6 @@ import ca.sait.lab4.models.User;
  */
 public class AccountService {
     public User login(String username, String password) {
-
         if (username.equals("abe") && password.equals("password")) {
         
             return new User(username, null);
